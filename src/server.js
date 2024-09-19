0f9c794e9fc1209dd3db1c00afed7db6a3cad92e
AliAksoyy <alifeyza4017@gmail.com>
2024-09-20 02:08:11 +0300

    mongoose connectd

--- src/server.js
+++ src/server.js
@@ -4,4 +4,31 @@ const cors = require("cors");
 const path = require("path");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const { router } = require("./routers");
 
+dotenv.config({ path: path.join(__dirname, "config", ".env") });
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.use(router);
+
+mongoose.connection.on("connection", () => {
+  console.log("mongodb connection succesfull");
+  startServer();
+});
+
+const startServer = () => {
+  app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+  });
+};
+
+mongoose.connect(process.env.MONGODB_URI);
