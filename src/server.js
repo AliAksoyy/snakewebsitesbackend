9ac4d2faeabe03cd0ff3b724a1c9cbc8303b81a6
AliAksoyy <alifeyza4017@gmail.com>
2024-09-20 01:58:57 +0300

    started

---
+++ src/server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
